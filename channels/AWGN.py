6860df049aeaa189e31f6ebd0e743125519a2d31
Pacco Tan <pacco.tan@integer.lsu.edu>
2025-11-16 17:11:16 -0600

    AWGN channel that modifies signal

---
+++ channels/AWGN.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+import math
+
+class AWGN(nn.Module):
+    def __init__(self, std: float | tuple[float,float]| None):
+        super(AWGN, self).__init__()
+        # Define std for complex gaussian noise
+        if type(std) == tuple:
+            self.std_I = std[0]
+            self.std_Q = std[1]
+        elif type(std) == float:
+            self.std_I = std*math.sqrt(0.5)
+            self.std_Q = std*math.sqrt(0.5)
+        else:
+            self.std_I = math.sqrt(0.5)
+            self.std_Q = math.sqrt(0.5)
+
+    def forward(self, x):
+        if torch.is_complex(x):
+            noise_I, noise_Q = torch.normal(0, std=self.std_I, size=x.size()), torch.normal(0,std=self.std_Q, size=x.size())
+            noise = torch.stack([noise_I, noise_Q], dim=-1)
+            x = x + torch.view_as_complex(noise)
+        else:
+            noise_I, noise_Q = torch.normal(0, std=self.std_I, size=x.size()[:-1]),torch.normal(0, std=self.std_Q[:-1], size=x.size())
+            noise = torch.stack([noise_I, noise_Q], dim=-1)
+            x = x + noise
+        return x
