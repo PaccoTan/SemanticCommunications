e676834ec07d64a02153f647db2223c8519f5439
Pacco Tan <pacco.tan@integer.lsu.edu>
2025-11-16 17:09:44 -0600

    Lookup table representing geoemetry of signals, enables lookup and reverse match to index

---
+++ model/GeometryTable.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class GeometryTable(nn.Module):
+    def __init__(self, geometry: torch.Tensor):
+        # geometry is a tensor of shape N,2 representing signal I and Q mappings for a corresponding Symbol
+        super(GeometryTable, self).__init__()
+        self.geometry = geometry
+        self.rev_geometry = {k: i for i,k in enumerate(geometry)}
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        signal = self.geometry[x]
+        return signal
+
+    def rev_lookup(self, x: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            # X is shape (B, *, E)
+            # Emb is shape (K, E)
+            shape = (1,) * (x.unsqueeze(-2).dim() - 2) + self.geometry.size()
+            dist = ((x.unsqueeze(-2) - self.geometry.view(shape)) ** 2).sum(-1)
+            ind = dist.min(dim=-1).indices
+        return ind
