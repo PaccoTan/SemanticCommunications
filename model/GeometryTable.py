62f40fdf1f631db6f0bbee27ff16450549e7f07e
Pacco Tan <tan.pacco@gmail.com>
2025-11-17 00:41:55 -0600

    added support for complex input

--- model/GeometryTable.py
+++ model/GeometryTable.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 class GeometryTable(nn.Module):
     def __init__(self, geometry: torch.Tensor):
         # geometry is a tensor of shape N,2 representing signal I and Q mappings for a corresponding Symbol
+        # if the tensor is complex then shape is N,1
         super(GeometryTable, self).__init__()
         self.geometry = geometry
         self.rev_geometry = {k: i for i,k in enumerate(geometry)}
@@ -17,6 +18,7 @@ class GeometryTable(nn.Module):
             # X is shape (B, *, E)
             # Emb is shape (K, E)
             shape = (1,) * (x.unsqueeze(-2).dim() - 2) + self.geometry.size()
-            dist = ((x.unsqueeze(-2) - self.geometry.view(shape)) ** 2).sum(-1)
+            dist = ((x.unsqueeze(-2) - self.geometry.view(shape)) *
+                    (x.unsqueeze(-2) - self.geometry.view(shape)).conj()).sum(-1).real
             ind = dist.min(dim=-1).indices
         return ind
