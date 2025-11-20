10f30bbfe5fd6aa304060c1ebb66dd0861fa85c8
Pacco Tan <tan.pacco@gmail.com>
2025-11-19 20:51:02 -0600

    code testing functionality of AWGN Channel implementation

---
+++ tests/AWGNTest.py
@@ -0,0 +1,53 @@
+import torch
+from matplotlib import pyplot as plt
+
+from channels.AWGN import AWGN
+import math
+from model.GeometryTable import GeometryTable
+
+
+K = 4
+rectangular_map = torch.cartesian_prod(
+    torch.linspace(int(-math.sqrt(K)+1),int(math.sqrt(K)-1),int(math.sqrt(K))),
+    torch.linspace(int(-math.sqrt(K)+1),int(math.sqrt(K)-1),int(math.sqrt(K)))
+)
+geometry = GeometryTable(rectangular_map)
+
+n = torch.distributions.normal.Normal(0,1)
+signal_error = torch.Tensor([0.1,0.01,0.001,0.0001])
+
+x = torch.randint(0,K,(10000,5,5))
+for error in signal_error:
+    variance = 1/(n.icdf(1-torch.sqrt(1-error))**2 * 0.5)
+    noise = AWGN(variance.sqrt().item())
+    noisy_x = noise(geometry(x))
+    noisy_points = noisy_x.view(-1,2)
+    plt.scatter(rectangular_map[:,0].numpy(), rectangular_map[:,1].numpy(),c='red',s=60,zorder=3)
+    plt.axhline(0, color='black', linewidth=1)
+    plt.axvline(0, color='black', linewidth=1)
+    plt.grid(True, linestyle='--', alpha=0.5)
+    plt.title(f"{K}-QAM Received Symbols with AWGN")
+    scatter = plt.scatter(noisy_points[:,0].numpy(), noisy_points[:,1].numpy(),c=x.view(-1).numpy(),marker='x',alpha=0.5)
+    cbar = plt.colorbar(scatter, ticks=range(len(x.unique())))
+    cbar.set_label('Symbol Label')
+    plt.show()
+    plt.close()
+    received_symbol = geometry.rev_lookup(noisy_x)
+    print(f"{K}-QAM with AWGN Channel Variance ={variance:.4f}")
+    print(f"Expected Accuracy: {((1-error)*100):.3f}%")
+    print(f"Actual Accuracy: {(received_symbol == x).sum()}/{x.numel()} = {((received_symbol == x).sum()/x.numel())*100:.2f}%")
+    print(f"SNR: {10*torch.log10(1/variance):.2f}dB")
+    print("-------------------------------------------------------------------------")
+
+print("\n\n\n\nResults using Complex Representation")
+geometry = GeometryTable(torch.view_as_complex(rectangular_map).unsqueeze(-1))
+for error in signal_error:
+    variance = 1/(n.icdf(1-torch.sqrt(1-error))**2 * 0.5)
+    noise = AWGN(variance.sqrt().item())
+    noisy_x = noise(geometry(x))
+    received_symbol = geometry.rev_lookup(noisy_x)
+    print(f"{K}-QAM with AWGN Channel Variance ={variance:.4f}")
+    print(f"Expected Accuracy: {((1-error)*100):.3f}%")
+    print(f"Actual Accuracy: {(received_symbol == x).sum()}/{x.numel()} = {((received_symbol == x).sum()/x.numel())*100:.2f}%")
+    print(f"SNR: {10*torch.log10(1/variance):.2f}dB")
+    print("-------------------------------------------------------------------------")
