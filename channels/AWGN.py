6cab4b099d5201d2b3ab5b40ecf12008c946a473
Pacco Tan <tan.pacco@gmail.com>
2025-11-25 19:10:37 -0600

    adding cuda support for noise and switched to distribution object

--- channels/AWGN.py
+++ channels/AWGN.py
@@ -7,22 +7,24 @@ class AWGN(nn.Module):
         super(AWGN, self).__init__()
         # Define std for complex gaussian noise
         if type(std) == tuple:
-            self.std_I = std[0]
-            self.std_Q = std[1]
+            std_I = std[0]
+            std_Q = std[1]
         elif type(std) == float:
-            self.std_I = std*math.sqrt(0.5)
-            self.std_Q = std*math.sqrt(0.5)
+            std_I = std*math.sqrt(0.5)
+            std_Q = std*math.sqrt(0.5)
         else:
-            self.std_I = math.sqrt(0.5)
-            self.std_Q = math.sqrt(0.5)
+            std_I = math.sqrt(0.5)
+            std_Q = math.sqrt(0.5)
+        self.I_noise = torch.distributions.Normal(0, std_I)
+        self.Q_noise = torch.distributions.Normal(0, std_Q)
 
-    def forward(self, x):
+    def forward(self, x, device="cuda"):
         if torch.is_complex(x):
-            noise_I, noise_Q = torch.normal(0, std=self.std_I, size=x.size()), torch.normal(0,std=self.std_Q, size=x.size())
-            noise = torch.complex(noise_I, noise_Q)
+            noise_I, noise_Q = self.I_noise.sample(x.size()), self.Q_noise.sample(x.size())
+            noise = torch.complex(noise_I, noise_Q).to(device)
             x = x + noise
         else:
-            noise_I, noise_Q = torch.normal(0, std=self.std_I, size=x.size()[:-1]),torch.normal(0, std=self.std_Q, size=x.size()[:-1])
-            noise = torch.stack([noise_I, noise_Q], dim=-1)
+            noise_I, noise_Q = self.I_noise.sample(x.size()[:-1]),self.Q_noise.sample(x.size()[:-1])
+            noise = torch.stack([noise_I, noise_Q], dim=-1).to(device)
             x = x + noise
         return x
